2b0abfa089c8795e08e2857e5ef1d541c3b3b272
Christos Tsolakis <tsolakic@zoho.com>
2014-04-28 18:58:40 +0300

    Create helix.py

---
+++ helix.py
@@ -0,0 +1,13 @@
+# Create the data.
+from numpy import pi, sin, cos, mgrid
+[u,v] = mgrid[-5:5:0.01,0:2*pi+0.1:0.1]
+a=2
+
+x = u*cos(v)
+y = u*sin(v)
+z = a*v
+K=-a**2/(u**2 +a**2)**2
+
+from mayavi import mlab
+s = mlab.mesh(x, y, z,scalars=K)
+mlab.show()
